acd975538bd2a3dcfd320c53cc51bebfde7d5ddb
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-07-03 21:11:40 -0700

    drawer navigation

--- src/app.js
+++ src/app.js
@@ -1,28 +1,32 @@
 import React from "react";
-import { View, Text } from 'react-native'
-import { StackNavigator } from 'react-navigation';
+import { View, Text, TouchableHighlight } from 'react-native'
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
+import SettingsScreen from "./screens/settings"
 
-class HomeScreen extends React.Component {
+class Home extends React.Component {
   static navigationOptions = {
+    drawerLabel: 'Home',
     title: 'Keeper',
   };
   render() {
-    return <Text>Hello, Navigation!</Text>;
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+      <Text>Hello, Navigation!</Text>
+      <TouchableHighlight onPress={() => navigate('Settings')}><Text style={{padding: 20}}>Settings</Text></TouchableHighlight>
+      </View>
+    )
   }
 }
 
-class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Settings'
-  }
+const MainNavigator = StackNavigator({
+  Home: { screen: Home },
+  Settings: { screen: SettingsScreen }
+})
 
-  render() {
-    return <Text>Settings</Text>
-  }
-}
-
-const App = StackNavigator({
-  Home: { screen: HomeScreen },
+const App = DrawerNavigator({
+  // DrawerNavigation is Home/Stats/Old Dreams
+  Home: { screen: MainNavigator },
   Settings: { screen: SettingsScreen }
 });
 
