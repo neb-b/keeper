ce1c52164688c948a9d3cead20d2ca4ae6c273af
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-07-15 12:48:44 -0700

    fix header import

--- src/screens/screen-wrapper.js
+++ src/screens/screen-wrapper.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { View, ScrollView } from 'react-native'
-import Header from '../components/header'
 
-const Screen = props => {
-  const { children, scroll } = props
+const Screen = ({ children, scroll }) => {
   const WrapperElement = scroll ? ScrollView : View
 
   return (
